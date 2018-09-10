a69f7afc6b5f82eab4430566a3d2882acba7c78d
lucasnnobrega <lucasnnobrega@gmail.com>
2018-09-10 20:58:57 -0300

    Criado MyPlot e outras coisas a mais

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -1,9 +1,12 @@
 import Header from './Header/Header'
 import Sidebar from './Sidebar/Sidebar'
 import TabularPage from './TabularPage/TabularPage'
+import MyPlot from './MyPlot/MyPlot'
+
 
 export {
   Header,
   Sidebar,
   TabularPage,
+  MyPlot
 }
